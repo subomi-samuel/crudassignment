d0f7b0b36796538212f9d37bebec3b916d9dfa68
Subomi-Samuel <subomiisamuel@gmail.com>
2021-05-15 09:53:25 +0100

    new update

--- routes/soldierRoutes.js
+++ routes/soldierRoutes.js
@@ -3,7 +3,7 @@ const soldierRouter = express.Router();
 const soldierController = require('../controllers/soldierControllers');
 
 
-soldierRouter.get('/soldier', soldierController.fetchSoldier);
+soldierRouter.get('/soldiers', soldierController.fetchSoldiers);
 soldierRouter.get('/soldier/:id', soldierController.fetchSoldier);
 soldierRouter.post('/soldier', soldierController.createNewSoldier);
 soldierRouter.put('/soldier/:id', soldierController.updateSoldier);
